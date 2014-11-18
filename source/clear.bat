b2e2814b8e0474f523e121a6162e27fa9053b1bc
mrpiggi <tudscr@gmail.com>
2014-11-18 19:38:38 +0100

    clear.bat sinnvoller

--- source/clear.bat
+++ source/clear.bat
@@ -4,10 +4,7 @@ attrib +h "*.dtx" /s
 attrib +h "*.ins" /s
 attrib +h "*.tex" /s
 attrib -h "*-temp.*" /s> nul
-attrib +h "doc\*.cls"
-attrib +h "doc\*.xdy"
-attrib +h "doc\tutorials\*.sty"
-attrib +h "logo\*.*"
+attrib +h "tudscrman.*" /sattrib +h "logo\*.*"
 attrib +h "test\*.cls"
 attrib +h "test\*.sty"
 del "*.*" /s /q> nul
