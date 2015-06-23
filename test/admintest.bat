3167a67e42389841638ad8bf556f1c04e623fc51
mrpiggi <tudscr@gmail.com>
2015-06-23 18:16:58 +0200

    ToDo: Windows-Skripte für Admin anpassen

---
+++ test/admintest.bat
@@ -0,0 +1,15 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate.
+    )
+
+    pause >nul
+    
