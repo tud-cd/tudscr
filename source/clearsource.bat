8893f6c1ff48ac46c93d747d224320feaeed8e8a
mrpiggi <hanisch.latex@outlook.com>
2018-07-10 16:30:11 +0200

    Anpassung an source für Open Sans

--- source/clearsource.bat
+++ source/clearsource.bat
@@ -3,9 +3,15 @@ attrib +h "*.bat" /s
 attrib +h "*.dtx" /s
 attrib +h "*.ins" /s
 attrib +h "tudscrsource.tex"
+attrib +h "doc\*.*" /s
+attrib +h "logo\*.eps"
+attrib +h "logo\*.pdf"
+attrib +h "font\createfonts.tex"
+attrib +h "font\ttf2afm.ff"
+attrib +h "font\etx\*.etx"
+attrib +h "font\mtx\*.mtx"
+attrib +h "font\external\*.*"
 attrib -h "install\*.bat" /s> nul
 attrib -h "install\*.tex" /s> nul
-attrib +h "doc\*.*" /s
-attrib +h "logo\*.*" /s
 del "*.*" /s /q> nul
 attrib -h "*.*" /s
