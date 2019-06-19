9a17caaa537a020ba100937a857e4273a588a9e4
mrpiggi <hanisch.latex@outlook.com>
2019-06-20 00:52:03 +0200

    Anpassungen an Paket opensans v2.0

--- source/clearsource.bat
+++ source/clearsource.bat
@@ -6,11 +6,6 @@ attrib +h "tudscrsource.tex"
 attrib +h "doc\*.*" /s
 attrib +h "logo\*.eps"
 attrib +h "logo\*.pdf"
-attrib +h "font\createfonts.tex"
-attrib +h "font\ttf2afm.ff"
-attrib +h "font\etx\*.etx"
-attrib +h "font\mtx\*.mtx"
-attrib +h "font\external\*.*"
 attrib -h "install\*.bat" /s> nul
 attrib -h "install\*.tex" /s> nul
 del "*.*" /s /q> nul
